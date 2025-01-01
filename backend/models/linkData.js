8e2fe8e88c77c33c0d815433273dd8eeebb32b25
jyotirmaygithub <jyotirmay2000gupta@gmail.com>
2025-01-01 23:59:36 +0530

    add key in the link data to have more data of link

--- backend/models/linkData.js
+++ backend/models/linkData.js
@@ -2,9 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const linkData = new Schema({
+    user_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
     url: {
         type: String,
         required: true,
+    },
+    linkUploadedDate: {
+        type: Date,
+        default: Date.now,
+    },
+    linkerName: {
+        type: String,
+        default: "Linker",
     },
     title: {
         type: String,
