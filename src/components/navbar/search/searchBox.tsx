3eaabf3c90b528cd657652c31675d7b50a1b3172
jyotirmaygithub <jyotirmay2000gupta@gmail.com>
2025-01-17 00:02:18 +0530

    fix: search box working as a modal

--- src/components/navbar/search/searchBox.tsx
+++ src/components/navbar/search/searchBox.tsx
@@ -4,21 +4,17 @@ interface SearchBoxProps {
     showSearchBox: boolean;
 }
 
-const SearchLogo:  React.FC<SearchBoxProps> = ({showSearchBox}) => {
+export default function SearchLogo () {
 
   return (
     <div className="absolute right-48 top-20">
-      {showSearchBox && (
         <input
           type="text"
           placeholder="Search..."
           className="border border-black p-2 ml-2"
           style={{ width: "400px" }}
         />
-      )}
     </div>
   );
 }
 
-
-export default SearchLogo
--- src/components/navbar/search/searchLogo.tsx
+++ src/components/navbar/search/searchLogo.tsx
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import SearchBox from "./searchBox";
+import { Modal, Box } from "@mui/material";
 
 export default function SearchLogo() {
-  const [showSearchBox, setShowSearchBox] = useState(false);
+  const [open, setOpen] = useState(false);
 
-  const handleIconClick = () => {
-    setShowSearchBox(!showSearchBox);
-  };
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <div>
-      <div onClick={handleIconClick} style={{ display: "inline-block" }}>
+      {/* Search Icon */}
+      <div
+        onClick={handleOpen}
+        style={{ display: "inline-block", cursor: "pointer" }}
+      >
         <svg
-          className="cursor-pointer"
           xmlns="http://www.w3.org/2000/svg"
           height="40px"
           viewBox="0 -960 960 960"
@@ -21,8 +24,13 @@ export default function SearchLogo() {
         >
           <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
         </svg>
-      </div>  
-      <SearchBox showSearchBox={showSearchBox} />
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <SearchBox />
+      </Modal>
     </div>
   );
 }
