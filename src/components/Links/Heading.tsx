6369d8d4b94b2ab3f15bb064c80f9af9d4e3de21
jyotirmaygithub <jyotirmay2000gupta@gmail.com>
2025-01-17 23:21:20 +0530

    heading for linker first page

---
+++ src/components/Links/Heading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Heading() {
+  return (
+    <div>
+      <h3>Links</h3>
+      <hr className='' />
+    </div>
+  )
+}
--- src/pages/linkUpload.tsx
+++ src/pages/linkUpload.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Nav from "../components/navbar/nav"
-import DataScrape from "../components/DataScraper/scraper"
+import React from "react";
+import Nav from "../components/navbar/nav";
+import DataScrape from "../components/DataScraper/scraper";
 
 export default function linkUpload() {
   return (
     <div>
-        <Nav/>
-      <DataScrape/>
+      <Nav />
+      <DataScrape />
     </div>
-  )
+  );
 }
