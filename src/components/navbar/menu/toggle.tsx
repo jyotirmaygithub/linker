4989efec8331b1846e61f5a5cddc1ee74d3550cc
jyotirmaygithub <jyotirmay2000gupta@gmail.com>
2025-02-14 22:30:32 +0530

    logout successful

--- src/components/navbar/menu/toggle.tsx
+++ src/components/navbar/menu/toggle.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
@@ -10,9 +9,17 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { clearAuthToken } from '../../../redux/authToken';
+import { clearUserData } from '../../../redux/userData';
+import { toast } from 'react-toastify';
 
 export default function RightSideDrawer() {
   const [isOpen, setIsOpen] = React.useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const toggleDrawer = (open: boolean) => 
     (event: React.KeyboardEvent | React.MouseEvent) => {
@@ -26,6 +33,16 @@ export default function RightSideDrawer() {
 
       setIsOpen(open);
     };
+
+  // Handle Logout Click
+  const handleLogout = () => {
+    navigate("/")
+    toast.success("Logged out successfully. See you again soon!");
+    dispatch(clearAuthToken())
+    dispatch(clearUserData())
+    // 
+    console.log("User clicked on Logout");
+  };
 
   const drawerList = () => (
     <Box
@@ -48,16 +65,24 @@ export default function RightSideDrawer() {
       </List>
       <Divider />
       <List>
-        {['Contact us', 'Logout'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Contact us" />
+          </ListItemButton>
+        </ListItem>
+
+        {/* Logout Button */}
+        <ListItem disablePadding>
+          <ListItemButton onClick={handleLogout}>
+            <ListItemIcon>
+              <LogoutIcon />
+            </ListItemIcon>
+            <ListItemText primary="Logout" />
+          </ListItemButton>
+        </ListItem>
       </List>
     </Box>
   );
